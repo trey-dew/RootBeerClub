c986f36d52398756229ff020c6802ebc40ac7c94
coolcatmeow <trey.dew@outlook.com>
2025-09-01 18:50:31 -0500

    fix

--- RootbeerClub_Server/server.js
+++ RootbeerClub_Server/server.js
@@ -23,8 +23,8 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    secure: false,
+    sameSite: 'lax',
     maxAge: 24 * 60 * 60 * 1000
   }
 }));
